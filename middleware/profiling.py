d1dbec2b6dddee66be1fe3e443402c31901f0b98
cbr work <cybergrind@gmail.com>
2011-03-18 14:18:25 +0200

    add support for callgraph plot with gprof2dot and dot

--- middleware/profiling.py
+++ middleware/profiling.py
@@ -4,8 +4,14 @@ import os
 import shutil
 import hotshot
 import hotshot.stats
-import cProfile
+from subprocess import call
+try:
+    import cProfile
+    NO_CPROFILE = False
+except:
+    NO_CPROFILE = True
 from django.conf import settings
+from django.http import HttpResponse
 from cStringIO import StringIO
 
 class ProfileMiddleware(object):
@@ -25,18 +31,18 @@ class ProfileMiddleware(object):
             if r.has_key('prof'):
                 self.tmpfile = tempfile.NamedTemporaryFile()
                 self.prof = hotshot.Profile(self.tmpfile.name)
-            elif r.has_key('cprof'):
+            elif not NO_CPROFILE and r.has_key('cprof'):
                 self.tmpfile = tempfile.NamedTemporaryFile()
                 self.prof = cProfile.Profile()
 
     def process_view(self, request, callback, callback_args, callback_kwargs):
         r = request.GET
-        if settings.DEBUG and (r.has_key('prof') or r.has_key('cprof')):
+        if settings.DEBUG and (r.has_key('prof') or (r.has_key('cprof') and not NO_CPROFILE)):
             return self.prof.runcall(callback, request, *callback_args, **callback_kwargs)
 
     def process_response(self, request, response):
         r = request.GET
-        if settings.DEBUG and (r.has_key('prof') or r.has_key('cprof')):
+        if settings.DEBUG and (r.has_key('prof') or (r.has_key('cprof') and not NO_CPROFILE)):
             out = StringIO()
             old_stdout = sys.stdout
             sys.stdout = out
@@ -55,7 +61,26 @@ class ProfileMiddleware(object):
             
             if request.GET.has_key('out'):
                 shutil.copy(self.tmpfile.name, os.path.join('/tmp', request.GET['out']))
-            if response and response.content and stats_str:
+                f = open(self.tmpfile.name)
+                response.content = f.read()
+            elif request.GET.has_key('graph'):
+                shutil.copy(self.tmpfile.name, '/tmp/graph.cprofile')
+                os.chmod('/tmp/graph.cprofile', 0666)
+                old = os.path.abspath('.')
+                os.chdir('/tmp')
+                cmd = '/usr/bin/gprof2dot -f pstats /tmp/graph.cprofile | /usr/bin/dot -Tsvg -o /tmp/graph.svg'
+                try:
+                    ex = call(cmd, shell=True)
+                except:
+                    ex = 'Error during call'
+                os.chdir(old)
+                if os.path.exists('/tmp/graph.svg'):
+                    content = open('/tmp/graph.svg').read()
+                    response = HttpResponse(mimetype='image/svg')
+                    response.content = content
+                else:
+                    response.content = ex
+            elif response and response.content and stats_str:
                 response.content = "<pre>" + stats_str + "</pre>"
 
         return response
